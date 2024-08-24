ea9ebadd0126415e5dc4f1f365e435cd266a2a73
Camila Ayala <camilaayala01@gmail.com>
2024-08-24 17:12:52 -0300

    agrego ej3

---
+++ validar-echo-server.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+docker run --rm -it --network=tp0_testing_net --name=validator ubuntu:20.04 bash -c '
+  apt-get update && apt-get install -y netcat
+
+  test_str="testing"
+  err_str="action: test_echo_server | result: fail"
+  success_str="action: test_echo_server | result: success"
+  
+  result=$((echo "$test_str" | nc -v -w 3 server 12345 2>&1) || echo  $err_str) 
+  echo $result
+  if [[ "$result" == *"$test_str"* ]]; then
+    echo $success_str
+  else 
+    echo $err_str
+  fi
+'
