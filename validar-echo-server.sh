aca15b761efe7c9da59bbe8da8b63b153c2388fe
Camila Ayala <camilaayala01@gmail.com>
2024-09-02 14:42:24 -0300

    modifico validador

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -8,7 +8,6 @@ docker run --rm -it --network=tp0_testing_net --name=validator ubuntu:20.04 bash
   success_str="action: test_echo_server | result: success"
   
   result=$((echo "$test_str" | nc -v -w 3 server 12345 2>&1) || echo  $err_str) 
-  echo $result
   if [[ "$result" == *"$test_str"* ]]; then
     echo $success_str
   else 
