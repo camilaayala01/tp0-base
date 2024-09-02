160e5c1c8a34706dc40f91c88aec77840e6eb49a
Camila Ayala <camilaayala01@gmail.com>
2024-09-02 16:41:00 -0300

    incorporo cambios hechos por tests

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-docker run --rm -it --network=tp0_testing_net --name=validator ubuntu:20.04 bash -c '
-  apt-get update && apt-get install -y netcat
+docker run --rm  --network=tp0_testing_net --name=validator ubuntu:20.04 bash -c '
+  apt-get update &>/dev/null && apt-get install -y netcat &>/dev/null
 
   test_str="testing"
   err_str="action: test_echo_server | result: fail"
   success_str="action: test_echo_server | result: success"
   
-  result=$((echo "$test_str" | nc -v -w 3 server 12345 2>&1) || echo  $err_str) 
-  echo $result
-  if [[ "$result" == *"$test_str"* ]]; then
+  result=$((echo "$test_str" | nc -w 3 server 12345) || echo  $err_str) 
+  if [[ "$result" == "$test_str" ]]; then
     echo $success_str
   else 
     echo $err_str
